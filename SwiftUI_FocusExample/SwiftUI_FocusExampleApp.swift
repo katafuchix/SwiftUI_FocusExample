891eb4cf1950342edacfe293dbcbced2370b40bf
katafuchix <katafuchix@gmail.com>
2024-03-15 14:14:09 +0900

    Initial Commit

---
+++ SwiftUI_FocusExample/SwiftUI_FocusExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_FocusExampleApp.swift
+//  SwiftUI_FocusExample
+//
+//  Created by cano on 2024/03/15.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_FocusExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
